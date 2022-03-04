1dda2d033d2d390a3fa53d17250aa26428d7bac7
kennshii <91844100+kennshii@users.noreply.github.com>
2022-03-04 18:35:07 +0200

    Update Program.cs

--- Calculator de note/Program.cs
+++ Calculator de note/Program.cs
@@ -11,8 +11,7 @@ namespace Calculator_de_note
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-/*            Application.Run(new StartForm());*/
-            Application.Run(new ProfilUmanForm());
+            Application.Run(new StartForm());
 
 
         }
